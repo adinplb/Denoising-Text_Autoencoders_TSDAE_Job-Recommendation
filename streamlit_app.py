0eb876d140240123457185572732a5d52c77dc8f
Muhammad Adin Palimbani <madinpalimbani09@gmail.com>
2025-05-21 08:41:39 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,13 +20,6 @@ uploaded_file = st.file_uploader('Choose your .pdf file', type="pdf")
 if uploaded_file is not None:
     df = extract_data(uploaded_file)
 
-def extract_data(feed):
-    data = []
-    with pdfplumber.load(feed) as pdf:
-        pages = pdf.pages
-        for p in pages:
-            data.append(p.extract_tables())
-    return None # build more code to return a dataframe 
 
 
 
