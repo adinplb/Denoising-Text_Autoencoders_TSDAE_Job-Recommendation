7d9fbccec66854bba4c44fd4816a7f1178aab031
Muhammad Adin Palimbani <madinpalimbani09@gmail.com>
2025-05-21 08:20:38 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,6 @@ st.title('👨‍⚕️ Optimizing Job Recommendation Using TSDAE at Job2Vec')
 
 st.info('TSDAE menghasilkan representasi embedding yang lebih robust terhadap data pekerjaan yang tidak berlabel')
 
-df_jobs = pd.read_csv("https://raw.githubusercontent.com/adinplb/dp-machinelearning-ai/refs/heads/master/dataset/tech_jobs.csv")
-df_jobs
+
 
 
