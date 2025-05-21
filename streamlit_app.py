80782207ab5d7b7dd12ee3dad6ef59e4ff33e1a0
Muhammad Adin Palimbani <madinpalimbani09@gmail.com>
2025-05-21 07:13:10 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,4 @@ import streamlit as st
 
 st.title('👨‍⚕️ Optimizing Job Recommendation Using TSDAE at Job2Vec')
 
-st.write('Menghasilkan representasi embedding yang lebih 
-robust terhadap data pekerjaan yang tidak berlabel, 
-memiliki variasi istilah tinggi, serta mengandung ketidakterstrukturan dan 
-noise semantik, guna meningkatkan relevansi dalam sistem rekomendasi kerj')
+st.write('Menghasilkan representasi embedding yang lebih robust terhadap data pekerjaan yang tidak berlabel')
